0f85b728ac251647f72fa0a31a16d4b41757da84
urnamee <100465709+urnamee@users.noreply.github.com>
2022-06-23 13:02:02 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+╭─「 Donasi • GOPAY 」
+│ •  [085795005004]
+│ •  [085795005004]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
